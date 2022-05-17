ff39c478db6c14c2eb99f4f013285b8bcc01df74
krishnachaitanya7 <chaitanyaradon89@gmail.com>
2022-05-16 17:13:32 -0700

    sync

--- eval_ssd.py
+++ eval_ssd.py
@@ -16,12 +16,16 @@ from vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite, create
 
 
 parser = argparse.ArgumentParser(description="SSD Evaluation on VOC Dataset.")
-parser.add_argument('--net', default="vgg16-ssd",
-                    help="The network architecture, it should be of mb1-ssd, mb1-ssd-lite, mb2-ssd-lite or vgg16-ssd.")
+parser.add_argument(
+    "--net",
+    default="mb1-ssd",
+    help="The network architecture, it should be of mb1-ssd, mb1-ssd-lite, mb2-ssd-lite or vgg16-ssd.",
+)
 parser.add_argument("--trained_model", type=str)
 
-parser.add_argument("--dataset_type", default="voc", type=str,
-                    help='Specify dataset type. Currently support voc and open_images.')
+parser.add_argument(
+    "--dataset_type", default="voc", type=str, help="Specify dataset type. Currently support voc and open_images."
+)
 parser.add_argument("--dataset", type=str, help="The root directory of the VOC dataset or Open Images dataset.")
 parser.add_argument("--label_file", type=str, help="The label file path.")
 parser.add_argument("--use_cuda", type=str2bool, default=True)
@@ -29,8 +33,7 @@ parser.add_argument("--use_2007_metric", type=str2bool, default=True)
 parser.add_argument("--nms_method", type=str, default="hard")
 parser.add_argument("--iou_threshold", type=float, default=0.5, help="The threshold of Intersection over Union.")
 parser.add_argument("--eval_dir", default="eval_results", type=str, help="The directory to store evaluation results.")
-parser.add_argument('--mb2_width_mult', default=1.0, type=float,
-                    help='Width Multiplifier for MobilenetV2')
+parser.add_argument("--mb2_width_mult", default=1.0, type=float, help="Width Multiplifier for MobilenetV2")
 args = parser.parse_args()
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() and args.use_cuda else "cpu")
 
@@ -55,7 +58,7 @@ def group_annotation_by_class(dataset):
                 all_gt_boxes[class_index][image_id] = []
             all_gt_boxes[class_index][image_id].append(gt_box)
             if class_index not in all_difficult_cases:
-                all_difficult_cases[class_index]={}
+                all_difficult_cases[class_index] = {}
             if image_id not in all_difficult_cases[class_index]:
                 all_difficult_cases[class_index][image_id] = []
             all_difficult_cases[class_index][image_id].append(difficult)
@@ -69,8 +72,9 @@ def group_annotation_by_class(dataset):
     return true_case_stat, all_gt_boxes, all_difficult_cases
 
 
-def compute_average_precision_per_class(num_true_cases, gt_boxes, difficult_cases,
-                                        prediction_file, iou_threshold, use_2007_metric):
+def compute_average_precision_per_class(
+    num_true_cases, gt_boxes, difficult_cases, prediction_file, iou_threshold, use_2007_metric
+):
     with open(prediction_file) as f:
         image_ids = []
         boxes = []
@@ -119,7 +123,7 @@ def compute_average_precision_per_class(num_true_cases, gt_boxes, difficult_case
         return measurements.compute_average_precision(precision, recall)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     eval_path = pathlib.Path(args.eval_dir)
     eval_path.mkdir(exist_ok=True)
     timer = Timer()
@@ -127,38 +131,38 @@ if __name__ == '__main__':
 
     if args.dataset_type == "voc":
         dataset = VOCDataset(args.dataset, is_test=True)
-    elif args.dataset_type == 'open_images':
+    elif args.dataset_type == "open_images":
         dataset = OpenImagesDataset(args.dataset, dataset_type="test")
 
     true_case_stat, all_gb_boxes, all_difficult_cases = group_annotation_by_class(dataset)
-    if args.net == 'vgg16-ssd':
+    if args.net == "vgg16-ssd":
         net = create_vgg_ssd(len(class_names), is_test=True)
-    elif args.net == 'mb1-ssd':
+    elif args.net == "mb1-ssd":
         net = create_mobilenetv1_ssd(len(class_names), is_test=True)
-    elif args.net == 'mb1-ssd-lite':
+    elif args.net == "mb1-ssd-lite":
         net = create_mobilenetv1_ssd_lite(len(class_names), is_test=True)
-    elif args.net == 'sq-ssd-lite':
+    elif args.net == "sq-ssd-lite":
         net = create_squeezenet_ssd_lite(len(class_names), is_test=True)
-    elif args.net == 'mb2-ssd-lite':
+    elif args.net == "mb2-ssd-lite":
         net = create_mobilenetv2_ssd_lite(len(class_names), width_mult=args.mb2_width_mult, is_test=True)
     else:
         logging.fatal("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
         parser.print_help(sys.stderr)
-        sys.exit(1)  
+        sys.exit(1)
 
     timer.start("Load Model")
     net.load(args.trained_model)
     net = net.to(DEVICE)
     print(f'It took {timer.end("Load Model")} seconds to load the model.')
-    if args.net == 'vgg16-ssd':
+    if args.net == "vgg16-ssd":
         predictor = create_vgg_ssd_predictor(net, nms_method=args.nms_method, device=DEVICE)
-    elif args.net == 'mb1-ssd':
+    elif args.net == "mb1-ssd":
         predictor = create_mobilenetv1_ssd_predictor(net, nms_method=args.nms_method, device=DEVICE)
-    elif args.net == 'mb1-ssd-lite':
+    elif args.net == "mb1-ssd-lite":
         predictor = create_mobilenetv1_ssd_lite_predictor(net, nms_method=args.nms_method, device=DEVICE)
-    elif args.net == 'sq-ssd-lite':
-        predictor = create_squeezenet_ssd_lite_predictor(net,nms_method=args.nms_method, device=DEVICE)
-    elif args.net == 'mb2-ssd-lite':
+    elif args.net == "sq-ssd-lite":
+        predictor = create_squeezenet_ssd_lite_predictor(net, nms_method=args.nms_method, device=DEVICE)
+    elif args.net == "mb2-ssd-lite":
         predictor = create_mobilenetv2_ssd_lite_predictor(net, nms_method=args.nms_method, device=DEVICE)
     else:
         logging.fatal("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
@@ -175,25 +179,28 @@ if __name__ == '__main__':
         boxes, labels, probs = predictor.predict(image)
         print("Prediction: {:4f} seconds.".format(timer.end("Predict")))
         indexes = torch.ones(labels.size(0), 1, dtype=torch.float32) * i
-        results.append(torch.cat([
-            indexes.reshape(-1, 1),
-            labels.reshape(-1, 1).float(),
-            probs.reshape(-1, 1),
-            boxes + 1.0  # matlab's indexes start from 1
-        ], dim=1))
+        results.append(
+            torch.cat(
+                [
+                    indexes.reshape(-1, 1),
+                    labels.reshape(-1, 1).float(),
+                    probs.reshape(-1, 1),
+                    boxes + 1.0,  # matlab's indexes start from 1
+                ],
+                dim=1,
+            )
+        )
     results = torch.cat(results)
     for class_index, class_name in enumerate(class_names):
-        if class_index == 0: continue  # ignore background
+        if class_index == 0:
+            continue  # ignore background
         prediction_path = eval_path / f"det_test_{class_name}.txt"
         with open(prediction_path, "w") as f:
             sub = results[results[:, 1] == class_index, :]
             for i in range(sub.size(0)):
                 prob_box = sub[i, 2:].numpy()
                 image_id = dataset.ids[int(sub[i, 0])]
-                print(
-                    image_id + "\t" + " ".join([str(v) for v in prob_box]).replace(" ", "\t"),
-                    file=f
-                )
+                print(image_id + "\t" + " ".join([str(v) for v in prob_box]).replace(" ", "\t"), file=f)
     aps = []
     print("\n\nAverage Precision Per-class:")
     for class_index, class_name in enumerate(class_names):
@@ -206,12 +213,9 @@ if __name__ == '__main__':
             all_difficult_cases[class_index],
             prediction_path,
             args.iou_threshold,
-            args.use_2007_metric
+            args.use_2007_metric,
         )
         aps.append(ap)
         print(f"{class_name}: {ap}")
 
     print(f"\nAverage Precision Across All Classes: {sum(aps)/len(aps)}")
-
-
-
